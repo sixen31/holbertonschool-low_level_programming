fc7913165042be75baa0db87756b82b8c97a3a19
Alexis <alexisgrandjean@hotmail.fr>
2023-02-20 05:46:12 -0800

    The last digit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 
-int main() 
+/* betty style doc for function main goes there */
+int main(void) 
 {
     int n;
-    srand(time(NULL));
-    n = rand() % 201 - 100; /* generates a random number between -100 and 100 */
-    printf("%d ", n);
+    srand(time(0));
 
     if (n > 0) {
         printf("is positive\n");
-    } else if (n == 0) {
+    } if (n == 0) {
         printf("is zero\n");
-    } else {
+    } if (n < 0) {
         printf("is negative\n");
     }
 
