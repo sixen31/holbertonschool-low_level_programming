8bcf42ef128e2cbc249256ac728ffceb97597354
Alexis <alexisgrandjean@hotmail.fr>
2023-02-20 05:03:15 -0800

    hello

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+    int n = rand() % 201 - 100; // generates a random number between -100 and 100
+    printf("%d ", n);
+
+    if (n > 0) {
+        printf("is positive\n");
+    } else if (n == 0) {
+        printf("is zero\n");
+    } else {
+        printf("is negative\n");
+    }
+
+    return 0;
+}
