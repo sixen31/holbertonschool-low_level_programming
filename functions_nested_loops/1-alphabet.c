c0f201fcbea13f5ab7283d779e3304118f1df140
Alexis <alexisgrandjean@hotmail.fr>
2023-02-22 01:27:39 -0800

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * void print_alphabet - check the code
  *
- * Return: Always 0.
+ *
  */
 void print_alphabet(void)
 {
@@ -13,6 +13,6 @@ while (letter <= 'z')
 {
 _putchar(letter);
 letter++;
-} 
+}
 _putchar('\n');
 }
