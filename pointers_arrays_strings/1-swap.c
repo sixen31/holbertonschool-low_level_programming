ba4cd301d09809e463ec32f8d6dca9a0637b2b69
Alexis <alexisgrandjean@hotmail.fr>
2023-02-27 02:45:22 -0800

    Battery st

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * @brief Swaps the values of two integers.
+ * swap_int - Swaps the values of two integers.
  *
  * This function swaps the values of the integers pointed to by a and b.
  *
- * @param[in,out] a Pointer to the first integer.
- * @param[in,out] b Pointer to the second integer.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
  *
  * @return None.
  */
