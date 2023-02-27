c1a0dde712ff122ea6bd84a0eca489bf7a8ee358
Alexis <alexisgrandjean@hotmail.fr>
2023-02-27 07:27:16 -0800

    Write a function that prints a string, followed by a new line, to stdout

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * _puts - prints a string to stdout, followed by a newline character
- * @str: the string to print
+ *@str: the string to print
  */
 void _puts(char *str)
 {
