0db92ba6f387ea8a2bfd962cac060b9ffef1d179
Alexis <alexisgrandjean@hotmail.fr>
2023-02-28 00:26:16 -0800

    Write a function that prints n elements of an array of integers, followed by a new line

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * Prints the first n elements of an array of integers, separated by commas.
+ *
+ *@param a The array to print.
+ *@param n The number of elements to print.
+ */
 void print_array(int *a, int n)
 {
 int i;
