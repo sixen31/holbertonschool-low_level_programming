cfd1f28f91cc2e198e564c3e27601d2a9925fdcd
Alexis <alexisgrandjean@hotmail.fr>
2023-03-01 00:21:46 -0800

    Write a function that prints every other character of a string, starting with the first character, followed by a new line

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * puts2 - print every other character starting from the 1st.
  * @str: input string.
  */
 void puts2(char *str)
 {
-int i = 0;
+int x;
 
-while (str[i] != '\0')
+for (x = 0; str[x] != '\0'; x++)
 {
-putchar(str[i]);
-i += 2;
+if (x % 2 == 0)
+{
+_putchar(str[x]);
 }
-
-putchar('\n');
+}
+_putchar('\n');
 }
