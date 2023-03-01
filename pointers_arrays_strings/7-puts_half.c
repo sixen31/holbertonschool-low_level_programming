c3fc2a0bee65295415a7b2cbadeef7be5378f070
Alexis <alexisgrandjean@hotmail.fr>
2023-03-01 00:26:02 -0800

    test

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,21 +2,8 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * puts_half - Prints the second half of a null-terminated string.
- *
- *@str: Pointer to the null-terminated string to print.
- *
- * This function prints the second half of the string pointed to by @str,
- *
- * followed by a newline character. If the length of the string is even,
- *
- * the first character of the second half is the middle character.
- *
- * If the length is odd, the first character of the second half is the
- *
- * character immediately after the middle character.
- *
- * Return: void
+ * puts_half - prints half of a string, followed by a new line
+ * @str: input string.
  */
 void puts_half(char *str)
 {
