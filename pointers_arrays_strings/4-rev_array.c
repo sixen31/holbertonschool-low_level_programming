90e721c2769aa4afc74c765b8781cc632057fffb
Alexis <alexisgrandjean@hotmail.fr>
2023-03-02 05:24:42 -0800

    test

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,39 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * reverse_array - Reverses the content of an array of integers.
- *@a: The array to modify.
- *@n: The number of elements of the array.
- *
- * Return: Nothing.
+ * @a: The array of integers.
+ * @n: The number of elements in the array.
  */
 void reverse_array(int *a, int n)
 {
-int i, temp;
+    int i, tmp;
 
-for (i = 0; i < n / 2; i++)
-{
-temp = a[i];
-a[i] = a[n - 1 - i];
-a[n - 1 - i] = temp;
+    for (i = 0; i < n / 2; i++)
+    {
+        tmp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = tmp;
+    }
 }
+
+/**
+ * print_array - Prints an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elements in the array.
+ */
+void print_array(int *a, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+        if (i < n - 1)
+        {
+            printf(", ");
+        }
+    }
+    printf("\n");
 }
