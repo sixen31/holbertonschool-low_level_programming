88f63387786fe86a8a6d9644d642cb2136631c5b
sixen31 <alexisgrandjean@hotmail.fr>
2023-03-20 03:02:45 -0700

    popyage

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,19 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure representing a dog
+ * @name: a pointer to the dog's name
+ * @age: the dog's age
+ * @owner: a pointer to the dog's owner's name
+ *
+ * Description: This structure represents a dog with a name, age, and owner.
+ */
+struct dog
+{
 char *name;
 float age;
 char *owner;
 };
+
+#endif /* DOG_H */
