52e23dd0e3ba42e350fceaaca89a839b3e97cae8
sixen31 <alexisgrandjean@hotmail.fr>
2023-03-22 09:21:26 -0700

    Beauty is variable, ugliness is constant

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
