7b14d4d0537d6491f3f474d8b18c53c541c4aadf
sixen31 <alexisgrandjean@hotmail.fr>
2023-03-24 11:58:07 -0700

    Write a function that prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,90 +1,58 @@
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_char - print char type element from va_list
- * @list: va_list passed to function
- */
-void print_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-
-/**
- * print_int - print int type element from va_list
- * @list: va_list passed to function
- */
-void print_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * print_str - print string element from va_list
- * @list: va_list passed to function
- */
-void print_str(va_list list)
-{
-	char *string;
-
-	string = va_arg(list, char*);
-	if (string == NULL)
-	{
-		string = "(nil)";
-	}
-	printf("%s", string);
-}
-
-/**
- * print_float - print float type element from va_list
- * @list: va_list passed to function
- */
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
- * print_all - print anything passed if char, int, float, or string.
- * @format: string of formats to use and print
+ * print_all - Prints anything according to format.
+ * @format: A list of types of arguments passed to the function.
+ *          c: char
+ *          i: integer
+ *          f: float
+ *          s: char * (if the string is NULL, print (nil) instead)
+ *          Any other char should be ignored.
+ *
+ * Return: void.
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i, j;
-	char *s;
-	va_list list;
+	va_list args;
+	int i = 0;
+	char *str;
 
-	checker storage[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_str},
-		{'\0', NULL}
-	};
+	va_start(args, format);
 
-	i = 0;
-	j = 0;
-	s = "";
-
-	va_start(list, format);
-	while (format != NULL && format[i] != '\0')
+	while (format && format[i])
 	{
-		while (j < 4)
+		switch (format[i])
 		{
-			if (format[i] == *storage[j].type)
-			{
-				printf("%s", s);
-				storage[j].function(list);
-				s = ", ";
-			}
-			j++;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
 		}
+
+		if (format[i + 1])
+			printf(", ");
 		i++;
-		j = 0;
 	}
 
+	va_end(args);
 	printf("\n");
-	va_end(list);
 }
