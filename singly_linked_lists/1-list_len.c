b5bc914d2ef183583164e3ed381ee994600a0974
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-04 06:41:47 -0700

    betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,16 +1,15 @@
 #include "lists.h"
 
-#include "lists.h"
-/** 
- * list_len - returns the number of elements in a linked list_t list 
+/**
+ * list_len - returns the number of elements in a linked list_t list
  * @h: A pointer to the head of the linked list
- * 
+ *
  * Return: Returns the number of elements in the linked list
  */
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
-	while (h != NULL)
+	while(h != NULL)
 	{
 		n++;
 		h = h->next;
