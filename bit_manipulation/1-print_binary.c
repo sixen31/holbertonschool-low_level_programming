1df336986c4c934b70b8edeeda8dc792f0b20ade
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-07 05:25:23 -0700

    Correction bad print and ulong_max

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,26 +1,14 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: the number to print
+ * print_binary - Prints the binary representation of a number.
+ *
+ * @n: The number to print.
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
-	int len = 0;
-		len++;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (len == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	for (len--; len >= 0; len--)
-	{
-		if ((n & (mask << len)) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-	}
+	_putchar((n & 1) + '0');
 }
