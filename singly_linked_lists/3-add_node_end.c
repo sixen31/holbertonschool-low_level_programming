2c39d7a0f77d976f256459e22b615846f00af24b
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-07 01:25:24 -0700

    correction betty 3-add_none_end.C

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,30 +9,32 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *current_node;
+	list_t *new_node, *current_node;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        free(new_node);
-        return (NULL);
-    }
-    new_node->len = strlen(str);
-    new_node->next = NULL;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-    if (*head == NULL) {
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    current_node = *head;
-    while (current_node->next != NULL)
-        current_node = current_node->next;
+	current_node = *head;
+	while (current_node->next != NULL)
+		current_node = current_node->next;
 
-    current_node->next = new_node;
+	current_node->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
