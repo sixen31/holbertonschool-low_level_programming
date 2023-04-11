fe1498dfbec5c8bc7fe96e1aa98522a70abf5043
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-11 04:49:18 -0700

    Modification betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ * read_textfile - reads text content from a file
+ *@filename: the name of the file to read
+ *@letters: the maximum number of characters to read from the file
+ *Return: the number of bytes written to standard output, or 0 on failure
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -9,17 +15,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (!filename)
-		return 0;
+		return (0);
 
 	buffer = malloc(letters);
 	if (!buffer)
-		return 0;
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		free(buffer);
-		return 0;
+		return (0);
 	}
 
 	bytes_read = read(fd, buffer, letters);
@@ -27,7 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		close(fd);
-		return 0;
+		return (0);
 	}
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
@@ -35,10 +41,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		close(fd);
-		return 0;
+		return (0);
 	}
 
 	free(buffer);
 	close(fd);
-	return bytes_written;
+	return (bytes_written);
 }
