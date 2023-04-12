4c9293c01145e7b3e51872425115a9373b45fd07
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-12 04:44:05 -0700

    Modification Betty style

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,8 +1,16 @@
 #include "hash_tables.h"
+/**
+ * key_index - Returns the index of a key in a hash table
+ * @key: The key to find the index of
+ * @size: The size of the hash table array
+ *
+ * Return: The index of the key in the hash table array
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value = hash_djb2(key);
 	unsigned long int index = hash_value % size;
+
 	return (index);
 }
