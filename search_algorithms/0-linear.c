4e17fed7bb145a110871eb0d214646e3dc1388fb
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-25 01:29:16 -0700

    betty style

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array using linear search algorithm
+ * linear_search - Searches for a value an array using linear search algorithm
  *
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
